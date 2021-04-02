78680c47f01c949b4564dbc03c7c7dbc82768b1e
Andrei Felecan <80598263+andreifcn@users.noreply.github.com>
2021-04-02 09:56:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,180 +1,139 @@
-let canvas;
-let ctx;
-let h = 5;
-let w = 10;
-let x = 10;
-let y = 11;
-let direction;
-let currentDirection;
-let interval;
-let score = 0;
-let appleXPos;
-let appleYPos;
-let itemToAdd;
-let snakeSize = [];
 
-// initialize game's functions: the map, the apple, the key listener
+let snakePosition = 0, line = 0, col = 0, applePos;
+let snakeSize = ['0'];
+let score = -1, interval, direction;
 
-function gameInit() {
+function initGame() {
 
-    generateCanvas();
-    generateApple();
-    startGame();
-    document.getElementById('play').style.opacity = 0;
+    initBoard();
+    createApple();
+    actionBox();
 }
 
-// starts the 2D drawing engine and generates the map
+function initBoard() {
 
-function generateCanvas() {
+    document.getElementById('action-panel').style.visibility = 'visible';
+    document.getElementById('play').style.opacity = '0';
 
-    canvas = document.createElement('CANVAS');
-    canvas.setAttribute('id','game-board');
-    ctx = canvas.getContext('2d');
-    document.getElementById('action-panel').appendChild(canvas);
-}
-
-// function to generate apple parameters and location
-// used by snakeGrowth to grow the snake
-
-function generateApple() {
-
-    appleXPos = Math.floor(Math.random() * 280);
-    appleYPos = Math.floor(Math.random() * 100);
-    ctx.fillStyle = 'red';
-    ctx.fillRect(appleXPos, appleYPos, w - 5, h - 2);
-}
-
-// grows the snake every time it eats the apple
-// increments score and generates another apple on the map
-
-function snakeGrowth() {
-
-    if (appleXPos >= x - 5 && appleXPos <= x + 5) {
-        if (appleYPos >= y - 5 && appleYPos <= y + 5) {
-            ctx.clearRect(appleXPos, appleYPos, w, h);
-            score++;
-
-            document.getElementById('game-score').innerText =
-            'Score: ' + score.toString();
-
-            return generateApple();
-        }
-    } 
-}
-
-// ends the game if the snake goes outside the map grid
-
-function checkBoundaries() {
-
-    if (x > 290 || y > 145 || x < 0 || y < 0) {
-        return endGame();
+    for (let i = 0; i < 99; i++) {
+        let block = document.createElement('DIV');
+        block.id = i;
+        block.className = 'block';
+        document.getElementById('action-panel').appendChild(block);
     }
 }
 
-// checks the movement array, snake's direction and its size
-// ends the game if the snake bites himself
 
-function checkSelfDamage(itemToAdd, snakeSize) {
-  
-    for (let i = 0; i < snakeSize.length; i++) {
-        let item1 = JSON.stringify(itemToAdd);
-        let item2 = JSON.stringify(snakeSize[i]);
-        if (item1 === item2) {
-            return endGame();
-        }
+function createApple() {
+
+    applePos = Math.floor(Math.random() * 99);
+
+    score++;
+    document.getElementById('game-score').innerText = 'Score: ' + score;
+
+    return document.getElementById(applePos).style.backgroundColor = 'red';
+}
+
+
+function growSnake() {
+    if (snakePosition === applePos) {
+        document.getElementById(applePos).innerHTML = '';
+        snakeSize.push(snakePosition);
+        createApple();
     }
 }
 
-// function stops the game engine and displays the game over message
-// if any of the game rules are broken
 
 function endGame() {
 
-    let gameOverMessage = document.createElement('H1');
-    gameOverMessage.setAttribute('id', 'game-over-msg');
-    gameOverMessage.innerText = 'Game Over!';
-    document.getElementById('action-panel').appendChild(gameOverMessage);
-
-    document.getElementById('play').removeEventListener('keydown', function play(event) {});
+    document.getElementById('game-over-msg').style.visibility = 'visible';
     clearInterval(interval);
-    buttonObj = document.getElementById('play');
-    return buttonObj.remove();
+    removeEventListener('keydown', function setMovement(event) {});
+    return document.getElementById('action-panel').innerHTML = innerHTML;
 }
 
-// once the snake's direction has been set, this function starts drawing
-// the snake's movement and its body size
+
+function checkBody(snakeSize) {
+
+    if (snakeSize.indexOf(snakePosition) != -1) {
+        return endGame();
+    } 
+}
+
 
 function drawSnake() {
 
-    itemToAdd = [[x], [y]];
-    checkSelfDamage(itemToAdd, snakeSize);
+    snakeSize.push(snakePosition);
 
-    snakeSize.push([[x], [y]]);
+    document.getElementById(snakePosition).style.backgroundColor = '#bc8f8f';
+    document.getElementById(snakeSize[0]).style.backgroundColor = 'black';
 
-    ctx.fillStyle = '#bc8f8f';
-    ctx.fillRect(x, y, w, h);
+    snakeSize.shift();
+}
 
-    if (snakeSize.length > score + 3) {
-        let removeItems = snakeSize.shift();
-        ctx.clearRect(removeItems[0], removeItems[1], w, h);
+
+function checkBorder() {
+
+    if (col < 0 || col > 10 ||
+        line < 0 || line > 8) {
+            return endGame();
     }
 }
 
-// sets parameters for drawing using keyboard direction commands
-// this sets the snake's movement direction
-// and begins drawing
 
-function startGame() {
+function moveDirection(event) {
 
-    document.getElementById('play').addEventListener('keydown', function play(event){
-        
-        if (event.key === 'ArrowDown' && currentDirection != 'ArrowUp') {
-            
-            clearInterval(interval); 
-            currentDirection = event.key;
+    switch(event.key) {
+        case 'ArrowUp':
+            line--;
+            snakePosition -= 11;
+            break;
+
+        case 'ArrowDown':
+            line++;
+            snakePosition += 11;
+            break;
+
+        case 'ArrowLeft':
+            col--;
+            snakePosition--;
+            break;
+
+        case 'ArrowRight':
+            col++;
+            snakePosition++;
+            break; 
+    }
+}
+
+
+function checkDirection(event) {
+
+    if (event.key === 'ArrowDown' && direction === 'ArrowUp' ||
+    event.key === 'ArrowUp' && direction ===  'ArrowDown' ||
+    event.key === 'ArrowLeft' && direction === 'ArrowRight' ||
+    event.key === 'ArrowRight' && direction === 'ArrowLeft') {
+        return false;
+    }
+    return true;
+}
+
+
+function actionBox () {
+
+    window.addEventListener('keydown', function setMovement(event) {
+
+        if (checkDirection(event)) {
+            clearInterval(interval);
+            direction = event.key;
             interval = setInterval(function() {
-                checkBoundaries();
-                snakeGrowth();
-                y += 3;
+                moveDirection(event);
+                checkBody(snakeSize);
+                checkBorder();
+                growSnake();
                 drawSnake();
-                
-            }, 100); 
-
-        } else if (event.key === 'ArrowUp' && currentDirection != 'ArrowDown') {
-
-            clearInterval(interval); 
-            currentDirection = event.key;
-            interval = setInterval(function() {
-                checkBoundaries();
-                snakeGrowth();
-                y -= 3;
-                drawSnake();
-
-            }, 100);
-
-        } else if (event.key === 'ArrowRight' && currentDirection != 'ArrowLeft') {
-
-            clearInterval(interval); 
-            currentDirection = event.key;
-            interval = setInterval(function() {
-                checkBoundaries();
-                snakeGrowth();
-                x += 5;
-                drawSnake();
-
-            }, 100);
-
-        }  else if (event.key === 'ArrowLeft' && currentDirection != 'ArrowRight') {
-
-            clearInterval(interval); 
-            currentDirection = event.key;
-            interval = setInterval(function() {
-                checkBoundaries();
-                snakeGrowth();
-                x -= 5;
-                drawSnake();
-
-            }, 100);
+            }, 500);
         }
+
     });
 }
